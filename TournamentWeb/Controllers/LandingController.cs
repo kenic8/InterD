51f5e71db3b1d4e1126c27a4937c7d79d7e60608
KN <nicholas.kevin96@gmail.com>
2021-05-18 17:51:12 +0200

    Kevins
    ffff

--- TournamentWeb/Controllers/LandingController.cs
+++ TournamentWeb/Controllers/LandingController.cs
@@ -2,16 +2,71 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using TournamentWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TournamentWeb.Controllers
 {
     public class LandingController : Controller
     {
+        public Tournament Tournament;
+        public List<int> Attendeecount { get; } = new List<int>();
 
+        private TournamentWebContext _context;
+
+        private UserManager<AppUser> userManager;
+        public LandingController(TournamentWebContext context, UserManager<AppUser> userMgr)
+        {
+            _context = context;
+             userManager = userMgr;
+
+        }
+       
         public IActionResult Landingpage()
         {
-          
-            return View();
+            // tournaments first 8 sorted by date
+
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var queryT = _context.Tournament.Include(b => b.Teams).ThenInclude(u => u.Attendees).Take(8).OrderByDescending(c => c.TimeFrame.Date).ThenBy(c => c.TimeFrame.TimeOfDay).ToList();
+
+                foreach (var T in queryT)
+                {
+                    foreach (var att in T.Teams)
+                    {
+
+                        T.ParticipantsAmount += att.Attendees.Count;
+
+                    }
+                }
+                // tournaments first 20 of all
+
+                var queryF = _context.Tournament.Include(b => b.Teams).ThenInclude(u => u.Attendees).Take(20).ToList();
+
+                foreach (var P in queryF)
+                {
+                    P.ParticipantsAmount = 0;
+                    foreach (var fatt in P.Teams)
+                    {
+
+
+                        P.ParticipantsAmount += fatt.Attendees.Count;
+
+                    }
+                }
+
+                ViewBag.NewT = queryT;
+                ViewBag.NewF = queryF;
+                return View("LoggedInLanding", ViewBag);
+            }
+            else
+            {
+                return View("Landingpage");
+            }
         }
 
     }
